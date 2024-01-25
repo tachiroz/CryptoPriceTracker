bd2e15039b827ab2e03817373af5655dc4daebea
tachiroz <matvey.semenov2003@mail.ru>
2024-01-25 15:24:06 +0400

    bug fixes. mvp is ready

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ def main(page: ft.Page):
     page.theme_mode = "dark"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    user_data = ft.TextField(label='Введите название криптовалюты', width=400)
+    user_data = ft.TextField(label='Введите название криптовалютной пары (пример: BTCUSDT)', width=400)
     crypto_price = ft.Text('')
 
     def get_info(e):
         if len(user_data.value) < 2:
             return
 
-        url = 'https://api.binance.com/api/v3/ticker/price'
-        response = requests.get(url).json()
-        temp = response['price']
-        crypto_price.value = f'Цена на токен равна ' + str({temp})
-        page.update()
+        symbol = user_data.value.upper()
+        url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}'
 
+        try:
+            response = requests.get(url).json()
+            price = float(response['price'])
+            crypto_price.value = f'Цена на {symbol} равна ' + str(price)
+        except (KeyError, ValueError):
+            crypto_price.value = f'Не удалось получить информацию о цене для {symbol}'
+
+        page.update()
 
     def change_theme(e):
         if page.theme_mode == 'light':
@@ -27,7 +32,6 @@ def main(page: ft.Page):
         else:
             page.theme_mode = 'light'
         page.update()
-
 
     page.add(
         ft.Row(
@@ -39,7 +43,7 @@ def main(page: ft.Page):
         ),
         ft.Row([user_data], alignment=ft.MainAxisAlignment.CENTER),
         ft.Row([crypto_price], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([ft.ElevatedButton('Узнать', on_click=get_info)], alignment=ft.MainAxisAlignment.CENTER)
+        ft.Row([ft.FilledTonalButton('Узнать', on_click=get_info)], alignment=ft.MainAxisAlignment.CENTER)
     )
 
 

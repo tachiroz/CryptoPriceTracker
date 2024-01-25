01f219524707624d7701e75eeed2cbea069cc9b4
tachiroz <matvey.semenov2003@mail.ru>
2024-01-25 15:36:58 +0400

    do not need to enter USDT

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import flet as ft
 import requests
 
+# Словарь для использования котировок без USDT
+symbol_mapping = {
+    'BTC': 'BTCUSDT',
+    'ETH': 'ETHUSDT',
+    'BNB': 'BNBUSDT',
+    'XRP': 'XRPUSDT',
+    'ADA': 'ADAUSDT',
+    'AVAX': 'AVAXUSDT',
+    'APT': 'APTUSDT',
+    'ARB': 'ARBUSDT'
+}
+
 
 def main(page: ft.Page):
     page.title = "CryptoPriceTracker"
     page.theme_mode = "dark"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    user_data = ft.TextField(label='Введите название криптовалютной пары (пример: BTCUSDT)', width=400)
+    user_data = ft.TextField(label='Введите название котировки криптовалюты (пример: BTC)', width=400)
     crypto_price = ft.Text('')
 
     def get_info(e):
         if len(user_data.value) < 2:
             return
 
-        symbol = user_data.value.upper()
+        symbol = user_data.value.upper()  # Преобразование введеных символов в верхний регистр
+
+        if symbol in symbol_mapping:
+            symbol = symbol_mapping[symbol]
+
         url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}'
 
         try:
